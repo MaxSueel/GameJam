2c286c084d0ed0e5f0ba7e1bfe9ee07d0a2283e0
MaxSueel <maxgomes.s@gmail.com>
2022-09-26 23:03:19 -0300

    Projeto atualizado

---
+++ Assets/SCRIPTS/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float H = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(H * Time.deltaTime, 0, 0));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.up * 12;
+        }
+
+
+    }
+}
